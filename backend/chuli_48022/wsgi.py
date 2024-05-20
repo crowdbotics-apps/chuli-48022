8e2d173d9ed79780d5a9de05320c89f5fb03fed7
Crowdbotics <team@crowdbotics.com>
2024-05-20 11:57:19 +0000

    Initial commit

---
+++ backend/chuli_48022/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for chuli_48022 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "chuli_48022.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "chuli-48022",
+  url: "https://chuli-48022.botics.co",
+  api: "https://chuli-48022.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
